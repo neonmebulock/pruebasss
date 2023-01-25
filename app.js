a57cf958a7f9d598c747fc52b25645f7bd9f124f
Neon Mebulock <nmebulok@gmail.com>
2023-01-25 17:52:15 -0500

    ssss

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const mongoose = require('mongoose');
 const router = require('./src/routes/index');
 app.use('/', router);
 
-const uri = `mongodb+srv://keku:ontkugAVJutZsKG7@cluster0.uj02c5w.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.keku}:${process.env.ontkugAVJutZsKG7}@cluster0.uj02c5w.mongodb.net/${process.env.kekuSchoolDB}?retryWrites=true&w=majority`;
 require('./src/views/js/socket') (io);
 
 //conexion base de datos
